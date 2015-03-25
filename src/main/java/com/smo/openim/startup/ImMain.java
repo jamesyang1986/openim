4066f22a3236fc08600558edc7cb3cc3cd156f51
杨建军 <yang.jianjun@immomo.com>
2015-03-25 15:28:05 +0800

    code init

---
+++ src/main/java/com/smo/openim/startup/ImMain.java
@@ -0,0 +1,23 @@
+package com.smo.openim.startup;
+
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by jianjunyang on 15/3/25.
+ */
+public class ImMain {
+    private static Logger logger = Logger.getLogger(ImMain.class);
+
+    public static void main(String[] args) {
+        if (args == null || args.length < 1) {
+            logger.error("USAGE:port");
+            System.exit(-1);
+        }
+
+        Integer port = Integer.parseInt(args[0]);
+        logger.info(String.format("the server is start on {0}", port));
+
+
+    }
+}
